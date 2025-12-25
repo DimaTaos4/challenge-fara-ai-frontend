63eb0891ea86e42addd7df43851d90f590e8fd47
Dima <taominhquan2004@gmail.com>
2025-12-25 17:53:38 +0100

    add proj

--- src/pages/RetailerSelectPage/RetailerSelectPage.jsx
+++ src/pages/RetailerSelectPage/RetailerSelectPage.jsx
@@ -1,8 +1,13 @@
 import styles from "./RetailerSelectPage.module.css"
-
+import { useAuthToken } from "../../shared/hooks/useAuthToken"
 const RetailerSelectPage = () => {
+
+    const { username } = useAuthToken()
+
     return (
-        <h2>Select</h2>
+        <div className={styles.retSelPage}>
+            <h3>{username}, select one of retailers</h3>
+        </div>
     )
 }
 export default RetailerSelectPage
